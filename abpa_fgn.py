4936b5197b451428b05917d66108221076b74929
408534734 <408534734@qq.com>
2017-10-31 21:25:23 +0800

    写好了接口需求，如果觉得有什么缺陷可以一起改一改~

---
+++ abpa_fgn.py
@@ -0,0 +1,11 @@
+class Abpa:
+	def __init__(self):
+		#todo
+
+	def firstStep(self):
+		#todo
+		return fromX, fromY, toX, toY
+
+	def move(self, fromX, fromY, toX, toY):
+		# todo
+		return fromX, fromY, toX, toY
