2021cf12c03c0705c0211baf4d131ce88cbc4bee
pengbd <961862555@qq.com>
2017-11-19 19:17:16 +0800

    组装了xqwl的组件，搜索部分还没写
    wlModule里整合了两个类，一个是board棋盘类，有判断棋子走步合法与否，棋力高低等元组和函数，一个是position局势类，可以判断走步是否合法，判断是否将死等，现在需要在abpa里调用它们，实现搜索，晚饭后见

---
+++ abpa_xqwl.py
@@ -0,0 +1,14 @@
+import wlModule
+
+class Abpa:
+    def __init__(self, down = True):
+        self.down = down
+        self.iWin = False
+
+    def myStep():
+        return (fromX, fromY, toX, toY)
+
+    def opponentMove(fromX, fromY, toX, toY):
+        pass
+
+    
